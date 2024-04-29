fff8b142b6066f389d177402a3a1b4736b2c7606
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-04-29 08:47:15 +0500

    masalalar

--- static/js/index.js
+++ static/js/index.js
@@ -1,202 +1,235 @@
-const slideData = [
-    {
-        index: 0,
-        headline: 'New Fashion Apparel',
-        button: 'Shop now',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
-    },
-    {
-        index: 1,
-        headline: 'In The Wilderness',
-        button: 'Book travel',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
-    },
-    {
-        index: 2,
-        headline: 'For Your Current Mood',
-        button: 'Listen',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
-    },
-    {
-        index: 3,
-        headline: 'Focus On The Writing',
-        button: 'Get Focused',
-        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
-    }
-]
+        // 90-MASALA
+// let str = `space`;
+
+// function addSpace(str) {
+//     if (str.length != 0) {
+//         let str2 = String(str);
+//         let arr = str2.split('');
+//         let res = arr.join(' ')
+//         return res;
+//     } else {
+//         return `After all, it is empty`
+//     }
+// }
+
+// console.log(addSpace(str));
 
 
-// =========================
-// Slide
-// =========================
 
-class Slide extends React.Component {
-    constructor(props) {
-        super(props)
+        // 91-MASALA
+// function multipleOfSecond(arg1, arg2) {
+//     let max = arg1;
+//     if (max < arg2) {
+//         max = arg2
+//     }
+//     let counter = 0
+//     for (let i = max.toString().length; i > 0; i--) {
+//         counter++;
+//     }
+//     return counter; // Math.round(Math.random() * max)
+// }
 
-        this.handleMouseMove = this.handleMouseMove.bind(this)
-        this.handleMouseLeave = this.handleMouseLeave.bind(this)
-        this.handleSlideClick = this.handleSlideClick.bind(this)
-        this.imageLoaded = this.imageLoaded.bind(this)
-        this.slide = React.createRef()
-    }
-
-    handleMouseMove(event) {
-        const el = this.slide.current
-        const r = el.getBoundingClientRect()
-
-        el.style.setProperty('--x', event.clientX - (r.left + Math.floor(r.width / 2)))
-        el.style.setProperty('--y', event.clientY - (r.top + Math.floor(r.height / 2)))
-    }
-
-    handleMouseLeave(event) {
-        this.slide.current.style.setProperty('--x', 0)
-        this.slide.current.style.setProperty('--y', 0)
-    }
-
-    handleSlideClick(event) {
-        this.props.handleSlideClick(this.props.slide.index)
-    }
-
-    imageLoaded(event) {
-        event.target.style.opacity = 1
-    }
-
-    render() {
-        const { src, button, headline, index } = this.props.slide
-        const current = this.props.current
-        let classNames = 'slide'
-
-        if (current === index) classNames += ' slide--current'
-        else if (current - 1 === index) classNames += ' slide--previous'
-        else if (current + 1 === index) classNames += ' slide--next'
-
-        return (
-            <li
-                ref={this.slide}
-                className={classNames}
-                onClick={this.handleSlideClick}
-                onMouseMove={this.handleMouseMove}
-                onMouseLeave={this.handleMouseLeave}
-            >
-                <div className="slide__image-wrapper">
-                    <img
-                        className="slide__image"
-                        alt={headline}
-                        src={src}
-                        onLoad={this.imageLoaded}
-                    />
-                </div>
-
-                <article className="slide__content">
-                    <h2 className="slide__headline">{headline}</h2>
-                    <button className="slide__action btn2">{button}</button>
-                </article>
-            </li>
-        )
-    }
-}
+// console.log(multipleOfSecond(299, 41));
+// CHALA QOLDI
 
 
-// =========================
-// Slider control
-// =========================
 
-const SliderControl = ({ type, title, handleClick }) => {
-    return (
-        <button className={`btn2 btn2--${type}`} title={title} onClick={handleClick}>
-            <svg className="icon" viewBox="0 0 24 24">
-                <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-            </svg>
-        </button>
-    )
-}
+        // 98-MASALA
+// function canTriangle(side1, side2, side3) {
+//     if (side1 + side2 > side3) {
+//         if (side1 + side3 > side2) {
+//             if (side2 + side3 > side1) {
+//                 return `Yes, you can make a triangle from these sides`
+//             } else {
+//                 return `No, you can't make a triangle from these sides`
+//             }
+//         } else {
+//             return `No, you can't make a triangle from these sides`
+//         }
+//     } else {
+//         return `No, you can't make a triangle from these sides`
+//     }
+// }
 
-
-// =========================
-// Slider
-// =========================
-
-class Slider extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = { current: 0 }
-        this.handlePreviousClick = this.handlePreviousClick.bind(this)
-        this.handleNextClick = this.handleNextClick.bind(this)
-        this.handleSlideClick = this.handleSlideClick.bind(this)
-    }
-
-    handlePreviousClick() {
-        const previous = this.state.current - 1
-
-        this.setState({
-            current: (previous < 0)
-                ? this.props.slides.length - 1
-                : previous
-        })
-    }
-
-    handleNextClick() {
-        const next = this.state.current + 1;
-
-        this.setState({
-            current: (next === this.props.slides.length)
-                ? 0
-                : next
-        })
-    }
-
-    handleSlideClick(index) {
-        if (this.state.current !== index) {
-            this.setState({
-                current: index
-            })
-        }
-    }
-
-    render() {
-        const { current, direction } = this.state
-        const { slides, heading } = this.props
-        const headingId = `slider-heading__${heading.replace(/\s+/g, '-').toLowerCase()}`
-        const wrapperTransform = {
-            'transform': `translateX(-${current * (100 / slides.length)}%)`
-        }
-
-        return (
-            <div className='slider' aria-labelledby={headingId}>
-                <ul className="slider__wrapper" style={wrapperTransform}>
-                    <h3 id={headingId} class="visuallyhidden">{heading}</h3>
-
-                    {slides.map(slide => {
-                        return (
-                            <Slide
-                                key={slide.index}
-                                slide={slide}
-                                current={current}
-                                handleSlideClick={this.handleSlideClick}
-                            />
-                        )
-                    })}
-                </ul>
-
-                <div className="slider__controls">
-                    <SliderControl
-                        type="previous"
-                        title="Go to previous slide"
-                        handleClick={this.handlePreviousClick}
-                    />
-
-                    <SliderControl
-                        type="next"
-                        title="Go to next slide"
-                        handleClick={this.handleNextClick}
-                    />
-                </div>
-            </div>
-        )
-    }
-}
+// console.log(canTriangle(3, 4, 5));
 
 
-ReactDOM.render(<Slider heading="Example Slider" slides={slideData} />, document.getElementById('app'));
+
+        // 99-MASALA
+// function createArrBetweenNum(arg1, arg2) {
+//     let newArr = [];
+
+//     if (!isNaN(arg1) && !isNaN(arg2)) {
+//         if (arg1 < arg2) {
+//             for (let i = arg1 + 1; i < arg2; i++) {
+//                 newArr.push(i);
+//             }
+    
+//             return newArr;
+//         } else {
+//             for (let i = arg2 + 1; i < arg1; i++) {
+//                 newArr.push(i);
+//             }
+    
+//             return newArr;
+//         }
+//     } else {
+//         return `Arguments must be numbers`
+//     }
+// }
+
+// console.log(createArrBetweenNum(12, 9));
+
+
+
+        // 100-MASALA
+
+
+
+        // 101-MASALA
+// function sumNums(n) {
+//     let sum = 0;
+
+//     if (!isNaN(n)) {
+//         if (n > 0) {
+//             for (let i = 1; i <= n; i++) {
+//                 sum += i;
+//             }
+//         } else {
+//             for (let i = 1; i >= n; i--) {
+//                 sum += i;
+//             }
+//         }
+    
+//         return sum;
+//     } else {
+//         return `A number must be entered`
+//     }
+// }
+// console.log(sumNums(-3));
+
+
+
+        // 102-MASALA
+// function isBetweenMinMax(n, obj) {
+//     if (!isNaN(n) && !isNaN(obj.min) && !isNaN(obj.max)) {
+//         if (obj.min <= n && n <= obj.max) {
+//             return true
+//         } else {
+//             return false
+//         }
+//     } else {
+//         return `Error`;
+//     }
+// }
+
+// console.log(isBetweenMinMax(4, { min: 0, max: 5 }));
+
+
+
+        // 103-MASALA
+    // 1-usul
+// function toSquare(n) {
+//     let m = Math.pow(n, 2);
+
+//     let counter = 0;
+//     for (let i = 1; i <= n.toString().length; i++) {
+//         counter++
+//     }
+
+//     let zero = ``;
+//     for (let i = 1; i <= counter; i++) {
+//         zero += `0`;
+//     }
+
+//     if (eval(`m % 1${zero}`) == n) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(toSquare(36));
+
+
+    // 2-usul
+// function toSquare(n) {
+//     let m = Math.pow(n, 2)
+//     let res = m.toString().endsWith(n);
+//     return res;
+// }
+
+// console.log(toSquare(6));
+
+
+
+        // 104-MASALA
+    // 1-usul
+// let arr = [1, 2, 3, 4, 5] // [1, 3, 5, 7, 9]
+
+// function addIndex(arr) {
+//     let newArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr.push(arr[i] + i);
+//     }
+
+//     return newArr
+// }
+
+// console.log(addIndex(arr));
+
+
+    // 2-usul
+// function addIndex(arr) {
+//     let arr2 = arr.map((v, i) => {
+//         return v + i;
+//     });
+
+//     return arr2;
+// }
+
+// console.log(addIndex(arr));
+
+
+
+        // 105-MASALA
+// function drinksWithoutSugar(arr) {
+//     let arr2 = arr.filter((v) => {
+//         return v != `cola` && v != `fanta`;
+//     })
+
+//     return arr2;
+// }
+
+// console.log(drinksWithoutSugar(["fanta", "cola", "water"]));
+
+
+
+        // 106-MASALA
+// function counterDigits(n) {
+//     let counter = 0;
+//     if (!isNaN(n)) {
+//         for (let i = 0; i < n.toString().length; i++) {
+//             counter++;
+//         }
+//     } else {
+//         return `error`
+//     }
+
+//     return counter;
+// }
+
+// console.log(counterDigits(61217311514));
+
+
+
+        // 107-MASALA
+// function produceLargest(n) {
+//     if (!isNaN(n)) {
+//         return Number(n.toString().split('').sort((a, b) => b - a).join(''))
+//     } else {
+//         return `error`
+//     }
+// }
+
+// console.log(produceLargest(674));
